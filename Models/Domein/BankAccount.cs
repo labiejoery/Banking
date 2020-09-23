91ff98375a67475f2dc0eee9a8451a3b565a4d59
Joery Labie <joery.labie@student.hogent.be>
2020-09-23 09:27:51 +0200

    Add class BankAccount

---
+++ Models/Domein/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿namespace Banking.Models.Domein
+{
+    public class BankAccount
+    {
+
+        private readonly string _accountNumber;
+        private decimal _balance;
+
+
+        #region Properties
+        public string AccountNumber { get; }
+        public decimal Balance { get; set; } = 0M;
+        #endregion
+
+
+        #region constructor
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        public decimal GetBalance()
+        {
+            return 0M;
+        }
+        #endregion
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models.Domein;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,16 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount account = new BankAccount("123-45871578-12");
+            account.Deposit(200M);
+            DisplayBalance(account);
+            account.Withdraw(100M);
+            DisplayBalance(account);
+        }
+
+        private static void DisplayBalance(BankAccount account)
+        {
+            Console.WriteLine("Balance is " + account.Balance);
         }
     }
 }
